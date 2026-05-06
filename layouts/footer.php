cf1c609a3ce4cb2df01317e1d412b03b3321629e
Glenn-IT <glenard2308@gmail.com>
2026-05-06 15:32:25 +0800

    chore: remove unused script includes from footer.php

--- layouts/footer.php
+++ layouts/footer.php
@@ -3,9 +3,5 @@
 
     <!-- Bootstrap 5 JS Bundle -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- LocalStorage CRUD Manager (must load first) -->
-    <script src="<?php echo $basePath ?? ''; ?>assets/js/storage.js"></script>
-    <!-- Custom Scripts -->
-    <script src="<?php echo $basePath ?? ''; ?>assets/js/script.js"></script>
 </body>
 </html>
