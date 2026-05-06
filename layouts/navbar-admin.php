8788a0b614c65bf1fc5f43f55277056304098bca
Glenn-IT <glenard2308@gmail.com>
2026-05-06 14:55:42 +0800

    feat: display logged-in user's full name in admin navbar

--- layouts/navbar-admin.php
+++ layouts/navbar-admin.php
@@ -22,7 +22,7 @@
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
-                        <i class="bi bi-shield-lock-fill me-1"></i>Administrator
+                        <i class="bi bi-shield-lock-fill me-1"></i><?= htmlspecialchars($_SESSION['ojams_user']['full_name'] ?? 'Administrator') ?>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end">
                         <li>
